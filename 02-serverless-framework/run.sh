d8592abd09513a8da5380f701061bd5fd8783b4d
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-04-14 13:12:33 -0300

    Creating AWS Lambda with Serverless Framework

--- 02-serverless-framework/run.sh
+++ 02-serverless-framework/run.sh
@@ -3,3 +3,23 @@
 #1. Install serverless
 npm i -g serverless
 
+#2. Creating new project with serverless framework
+sls
+
+#3. Deploying
+sls deploy
+
+#4. Accessing generated endpoint
+https://ieoz61wjil.execute-api.us-east-1.amazonaws.com/dev/hello
+
+#5. We also can invoke lambda
+sls invoke -f hello
+
+#6. Invoke local
+sls invoke local -f hello --log
+
+#7. Configure dashboard
+sls
+
+#8. Showing logs
+sls logs -f hello --tail
