a44255ac5c285c642654b6e2d058ab3383071586
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-04-15 22:49:37 -0300

    Serverless image analysis - adding image

--- 03-image-analysis/image-analysis/handler.js
+++ 03-image-analysis/image-analysis/handler.js
@@ -1,12 +1,26 @@
 'use strict';
 
+const { promises: { readFile } } = require('fs');
+
 class Handler {
   constructor({ rekoSvc }){
     this.rekoSvc = rekoSvc
   }
 
+  async detectImageLabels(buffer) {
+    const result = await this.rekoSvc.detectLabels({
+      Image: {
+        Bytes: buffer
+      }
+    }).promise();
+
+    console.log('result: ', result);
+  }
+
   async main(event) {
     try {
+      const imgBuffer = await readFile('./images/teste.jpeg');
+      this.detectImageLabels(imgBuffer);
       return {
         statusCode: 200,
         body: 'Hi'
@@ -22,10 +36,10 @@ class Handler {
 }
 
 const aws = require('aws-sdk');
-const reko = aws.Rekognition();
+const reko = new aws.Rekognition();
 
 const handler = new Handler({
-  rekoSvc = reko
+  rekoSvc: reko
 });
 
 module.exports.main = handler.main.bind(handler)
