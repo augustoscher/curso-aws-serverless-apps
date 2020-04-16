f91540be0f7e342861c230a119ddb2ed11d3275c
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-04-16 20:38:47 -0300

    Adding endpoint to receive custom images

--- 03-image-analysis/run.sh
+++ 03-image-analysis/run.sh
@@ -6,3 +6,10 @@
 #2. Invoke local
 sls invoke local -f img-analysis
 
+#3. Invoke using path
+sls invoke local -f img-analysis --path request.json
+sls invoke -f img-analysis --path request.json --log
+
+#4. Invoke lambda
+https://drqa3kqxm2.execute-api.us-east-1.amazonaws.com/dev/analyse?imageUrl=https://www.cienciasresumos.com.br/wp-content/uploads/2018/12/curiosidades-sobre-os-coelhos.jpg
+https://drqa3kqxm2.execute-api.us-east-1.amazonaws.com/dev/analyse?imageUrl=https://s2.glbimg.com/slaVZgTF5Nz8RWqGrHRJf0H1PMQ=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg
