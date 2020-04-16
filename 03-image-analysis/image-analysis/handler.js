96daf28a59e52dfaadd1e1b3cd3ade973f0b8c3b
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-04-15 23:32:27 -0300

    Serverless image analysis - adding image

--- 03-image-analysis/image-analysis/handler.js
+++ 03-image-analysis/image-analysis/handler.js
@@ -3,8 +3,9 @@
 const { promises: { readFile } } = require('fs');
 
 class Handler {
-  constructor({ rekoSvc }){
-    this.rekoSvc = rekoSvc
+  constructor({ rekoSvc, translatorSvc }){
+    this.rekoSvc = rekoSvc;
+    this.translatorSvc = translatorSvc;
   }
 
   async detectImageLabels(buffer) {
@@ -14,13 +15,31 @@ class Handler {
       }
     }).promise();
 
-    console.log('result: ', result);
+    const workingItems = result.Labels.filter(({ Confidence }) => Confidence > 80)
+    const names = workingItems.map(({ Name }) => Name ).join(' and ');
+    return { names, workingItems };
+  }
+
+  async translateText(text) {
+    const params = {
+      SourceLanguageCode: 'en',
+      TargetLanguageCode: 'pt',
+      Text: text
+    }
+
+    return await this.translatorSvc.translateText(params).promise();
   }
 
   async main(event) {
     try {
       const imgBuffer = await readFile('./images/teste.jpeg');
-      this.detectImageLabels(imgBuffer);
+      console.log('Detecting labels...');
+      const { names, workingItems } = await this.detectImageLabels(imgBuffer);
+
+      console.log('Translate to portuguese...');
+      const translatedText = await this.translateText(names);
+
+      console.log('Handling final object...')
       return {
         statusCode: 200,
         body: 'Hi'
@@ -37,9 +56,11 @@ class Handler {
 
 const aws = require('aws-sdk');
 const reko = new aws.Rekognition();
+const translator = new aws.Translate();
 
 const handler = new Handler({
-  rekoSvc: reko
+  rekoSvc: reko,
+  translatorSvc: translator
 });
 
 module.exports.main = handler.main.bind(handler)
