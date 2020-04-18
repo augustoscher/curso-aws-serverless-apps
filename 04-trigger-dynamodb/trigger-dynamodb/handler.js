dba3f7917a7a5a6d87a63ab7329199b388fb15e0
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-04-18 15:44:51 -0300

    Trigger - DynamoDB - Trigger

--- 04-trigger-dynamodb/trigger-dynamodb/handler.js
+++ 04-trigger-dynamodb/trigger-dynamodb/handler.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  heroesInsert: require('./src/heroes-insert')
+  heroesInsert: require('./src/heroes-insert'),
+  heroesTrigger: require('./src/heroes.trigger')
 };
