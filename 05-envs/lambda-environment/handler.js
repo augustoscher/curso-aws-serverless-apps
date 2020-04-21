0147c6e95b4d2406129fa68253535c6949de1ae2
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-04-21 00:14:28 -0300

    Serverless - Envs

--- 05-envs/lambda-environment/handler.js
+++ 05-envs/lambda-environment/handler.js
@@ -2,12 +2,15 @@
 
 const settings = require('./config/serverless/settings');
 const axios = require('axios');
-
+const cheerio = require('cheerio')
 class Handler {
   static async main(event) {
     console.log('at ', new Date().toISOString, JSON.stringify(event, null, 2));
     const { data } = await axios.get(settings.commitMessageUrl);
-    console.log('Data: ', data);
+    
+    const $ = cheerio.load(data);
+    const [commitMessage] = await $("#content").text().trim().split('\n');
+    console.log('Data: ', commitMessage);
 
     return {
       statusCode: 200,
