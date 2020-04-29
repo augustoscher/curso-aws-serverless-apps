2f5fbf10a4a847b0ef08f5958c44b12f95bc93b3
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-04-29 00:16:45 -0300

    Serverless - AWS Layers - Mememaker

--- 06-lambda-layers/mememaker/app/handler.js
+++ 06-lambda-layers/mememaker/app/handler.js
@@ -1,15 +1,31 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
+const { exec } = require('child_process');
+const { promisify } = require('util');
+const shell = promisify(exec);
+
+class Handler {
+  constructor() {}
+
+  async main(event) {
+    const response = await shell(
+      'gm identify --verbose ./resources/img/homer.jpg'
+    )
+    console.log({ response })
+    try {
+      return {
+        statusCode: 200,
+        body: response.stdout,
+      };
+    } catch (error) {
+      console.error("error***", error.stack);
+      return {
+        statusCode: 500,
+        body: error.stack,
+      };
+    }
+  }
+}
+
+const handler = new Handler();
+module.exports = { mememaker: handler.main.bind(handler) }
