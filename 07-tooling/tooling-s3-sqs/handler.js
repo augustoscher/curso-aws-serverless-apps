76727ec6de5d6f4b97fa5c285d0dbee3cf5981cf
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-26 23:07:29 -0300

    07 - Tooling - S3 and SQS

--- 07-tooling/tooling-s3-sqs/handler.js
+++ 07-tooling/tooling-s3-sqs/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Hello!',
         input: event,
       },
       null,
