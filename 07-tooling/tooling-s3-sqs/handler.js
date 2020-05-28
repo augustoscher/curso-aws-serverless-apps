4c5f5a0bc77028d6f8d23d4dbe27141fb202f2c7
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-27 23:41:28 -0300

    07 - Tooling - S3, SQS, Localstack

--- 07-tooling/tooling-s3-sqs/handler.js
+++ 07-tooling/tooling-s3-sqs/handler.js
@@ -1,12 +1,24 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+
+const host = process.env.LOCALSTACK_HOST || "localhost";
+const s3Port = process.env.S3_PORT || "4572";
+const s3Config = {
+  s3ForcePathStyle: true,
+  endpoint: new AWS.Endpoint(`http://${host}:${s3Port}`)
+}
+
+const S3 = new AWS.S3(s3Config);
+
 module.exports.hello = async event => {
+  const allBuckets = await S3.listBuckets().promise();
+  console.log('found: ', allBuckets);
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Helloooo docker live reload!',
-        input: event,
+        allBuckets
       },
       null,
       2
