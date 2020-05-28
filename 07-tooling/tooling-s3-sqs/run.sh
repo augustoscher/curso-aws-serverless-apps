b9e0785a558ba38d2d797839f386f5dc06ef4598
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-27 23:45:42 -0300

    07 - Tooling - S3, SQS, Localstack

--- 07-tooling/tooling-s3-sqs/run.sh
+++ 07-tooling/tooling-s3-sqs/run.sh
@@ -15,3 +15,7 @@ docker-compose up -d localstack
 
 # Building docker-compose
 docker-compose up --build
+
+# Stop and remove images and volumes
+docker-compose down
+docker volume rm tooling-s3-sqs_nodemodules
