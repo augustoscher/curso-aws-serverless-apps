6500c7f57a5cee4dcb3a106f1ca8cd22c8fd12e3
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-31 22:09:12 -0300

    07 - Tooling - Streams and SQS

--- 07-tooling/sqs-s3-streams/run.sh
+++ 07-tooling/sqs-s3-streams/run.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Setup lambda locally
-sls offline
-
-# Calling hello word in another terminal
-curl http://localhost:3000/dev/hello
-
-# Creating and Run test
-sls create test -f hello
-yarn test
-
 # Running docker-compose for localstack
 docker-compose up -d localstack
 
