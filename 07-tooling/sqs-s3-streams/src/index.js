042c4c96878086194932c321bfc057167d5d2c31
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-31 22:31:22 -0300

    07 - Tooling - Streams and SQS

---
+++ 07-tooling/sqs-s3-streams/src/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  s3listener = require('./s3.listener'),
+  sqslistener = require('./sqs.listener')
+}
---
+++ 07-tooling/sqs-s3-streams/src/s3.listener.js
@@ -0,0 +1,20 @@
+class Handler {
+  async main(event) {
+    try{
+      return {
+        statusCode: 200,
+        body: 'Hello'
+      }
+
+    } catch(e) {
+      console.log('Error: ', e.stack);
+      return {
+        statusCode: 500,
+        body: 'Internal Error'
+      }
+    }
+  }
+}
+
+const handler = new Handler();
+module.exports = handler.main.bind(handler);
