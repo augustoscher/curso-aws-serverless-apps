1ef29e07c131e85b7ae1609f2c9e2d35910860ab
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-02 00:33:22 -0300

    07 - Tooling - Streams and SQS

--- 07-tooling/sqs-s3-streams/src/s3.listener.js
+++ 07-tooling/sqs-s3-streams/src/s3.listener.js
@@ -1,6 +1,28 @@
 // When S3 csv file is posted, this lambda will be called
+const AWS = require('aws-sdk');
+
 class Handler {
+  static getSdks() {
+    const host = process.env.LOCALSTACK_HOST || "localhost";
+    const s3Port = process.env.S3_PORT || "4572";
+    const sqsPort = process.env.SQS_PORT || "4576";
+    const isLocal = process.env.IS_LOCAL;
+    const s3Endpoint = new AWS.Endpoint(`http:${host}:${s3Port}`);
+    const sqsEndpoint = new AWS.Endpoint(`http:${host}:${sqsPort}`);
+
+    const s3Config = {
+      endpoint: s3Endpoint,
+      s3ForcePathStyle: true
+    }
+
+    const sqsConfig = {
+      endpoint: sqsEndpoint,
+    }
+  }
+
   async main(event) {
+
+
     console.log('**s3 event: ', JSON.stringify(event, null, 2))
     try{
       return {
