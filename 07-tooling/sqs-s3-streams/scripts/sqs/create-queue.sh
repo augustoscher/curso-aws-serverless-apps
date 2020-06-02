519fa5a00cd0c0edbee40c82884e5c00213f9629
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-02 00:05:13 -0300

    07 - Tooling - Streams and SQS

--- 07-tooling/sqs-s3-streams/scripts/sqs/create-queue.sh
+++ 07-tooling/sqs-s3-streams/scripts/sqs/create-queue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ./create-queue.sh my-queue 
+# ./create-queue.sh file-handler 
 
 # Create queue on sqs
 QUEUE_NAME=$1
@@ -8,8 +8,14 @@ QUEUE_NAME=$1
 aws \
   sqs create-queue \
   --queue-name $QUEUE_NAME \
-  --endpoint-url=http://localhost:4576
+  # --endpoint-url=http://localhost:4576
 
 aws \
   sqs list-queues \
-  --endpoint-url=http://localhost:4576
+  # --endpoint-url=http://localhost:4576
+
+# {
+#   "QueueUrls": [
+#       "https://queue.amazonaws.com/824273212766/file-handler"
+#   ]
+# }
