dae3d6ee4b7078cafff6c2682b26fca5f81587a6
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-02 23:36:08 -0300

    07 - Tooling - Streams and SQS

--- 07-tooling/sqs-s3-streams/src/s3.listener.js
+++ 07-tooling/sqs-s3-streams/src/s3.listener.js
@@ -4,7 +4,6 @@ const { Writable, pipeline } = require('stream');
 const csvtojson = require('csvtojson');
 
 class Handler {
-
   constructor({ s3Svc, sqsSvc }) {
     this.s3Svc = s3Svc;
     this.sqsSvc = sqsSvc;
@@ -21,13 +20,13 @@ class Handler {
 
     const s3Config = {
       endpoint: s3Endpoint,
-      s3ForcePathStyle: true
-    }
-    
+      s3ForcePathStyle: true,
+    };
+
     const sqsConfig = {
       endpoint: sqsEndpoint,
-    }
-    
+    };
+
     if (!isLocal) {
       delete s3Config.endpoint;
       delete sqsConfig.endpoint;
@@ -35,14 +34,16 @@ class Handler {
 
     return {
       s3: new AWS.S3(s3Config),
-      sqs: new AWS.SQS(sqsConfig)
-    }
+      sqs: new AWS.SQS(sqsConfig),
+    };
   }
 
   async getQueueUrl() {
-    const { QueueUrl } = await this.sqsSvc.getQueueUrl({
-      QueueName: this.queueName 
-    }).promise();
+    const { QueueUrl } = await this.sqsSvc
+      .getQueueUrl({
+        QueueName: this.queueName,
+      })
+      .promise();
     return QueueUrl;
   }
 
@@ -61,46 +62,57 @@ class Handler {
   processDataOnDemand(queueUrl) {
     const writableStream = new Writable({
       write: (chunk, encoding, done) => {
-        const item = chunk.toString()
-        console.log('received: ', item);
-        done()
-      }
+        const item = chunk.toString();
+        console.log("sending.. ", item, "at ", new Date().toISOString());
+
+        this.sqsSvc.sendMessage(
+          {
+            QueueUrl: queueUrl,
+            MessageBody: item,
+          },
+          done
+        );
+      },
     });
     return writableStream;
+  }
+
+  async pipefyStreams(...args) {
+    return new Promise((resolve, reject) => {
+      //args são as n functions
+      pipeline(...args, (error) => (error ? reject(error) : resolve()));
+    });
   }
 
   async main(event) {
     // console.log('**s3 event: ', JSON.stringify(event, null, 2));
     const { name, key } = this.getParamsFromEvent(event);
-    console.log('processing: ', name, key)
+    console.log("processing: ", name, key);
 
     try {
-      console.log('geting queueURL...')
+      console.log("geting queueURL...");
       const queueUrl = await this.getQueueUrl();
-      
-      // this.s3Svc.getObject({ Bucket: name, Key: key })
-      //   .createReadStream()
-      //   .on("data", msg => console.log('on data: ', msg.toString()))
-      //   .on("error", msg => console.log('on error: ', msg.toString()))
-      //   .on("close", msg => console.log('on close: ', msg.toString()))
-      //   .on("finish", msg => console.log('on finish'))
 
-      this.s3Svc.getObject({ Bucket: name, Key: key })
-        .createReadStream()
-        .pipe(csvtojson())
-        .pipe(this.processDataOnDemand(queueUrl))
+      //desta forma, o processo que ocasionar o erro primeiro, ja para a execução
+      //e não precisa pegar o onfinish, onclose para retornar o handler
+      await this.pipefyStreams(
+        this.s3Svc.getObject({ Bucket: name, Key: key }).createReadStream(),
+        csvtojson(),
+        this.processDataOnDemand(queueUrl)
+      );
+
+      console.log("process finished...", new Date().toISOString());
 
       return {
         statusCode: 200,
-        body: 'S3 oi'
-      }
-
-    } catch(e) {
-      console.log('Error: ', e.stack);
+        body: "S3 oi",
+      };
+    } catch (e) {
+      console.log("Error: ", e.stack);
       return {
         statusCode: 500,
-        body: 'Internal Error'
-      }
+        body: "Internal Error",
+      };
     }
   }
 }
@@ -111,3 +123,17 @@ const handler = new Handler({
   s3Svc: s3
 });
 module.exports = handler.main.bind(handler);
+
+
+
+// this.s3Svc.getObject({ Bucket: name, Key: key })
+//   .createReadStream()
+//   .on("data", msg => console.log('on data: ', msg.toString()))
+//   .on("error", msg => console.log('on error: ', msg.toString()))
+//   .on("close", msg => console.log('on close: ', msg.toString()))
+//   .on("finish", msg => console.log('on finish'))
+
+// this.s3Svc.getObject({ Bucket: name, Key: key })
+//   .createReadStream()
+//   .pipe(csvtojson())
+//   .pipe(this.processDataOnDemand(queueUrl))
