15e0a960edca4885468e49620a4ac03646aa9f39
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-03 23:48:00 -0300

    07 - Tooling - Streams and SQS

---
+++ 07-tooling/sqs-s3-streams/scripts/remove.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ./remove.sh arquivos-augusto-001
+
+BUCKET_NAME=$1
+
+
+echo "removing items from bucket..."
+aws  \
+  s3 rm s3://$BUCKET_NAME \
+  --recursive
+  # --endpoint-url=http://localhost:4572
+
+aws  \
+  s3 ls $BUCKET_NAME \
+  # --endpoint-url=http://localhost:4572
+
--- 07-tooling/sqs-s3-streams/scripts/setup.sh
+++ 07-tooling/sqs-s3-streams/scripts/setup.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-# ./setup.sh arquivos-augusto-001 file-handler
+# ./scripts/setup.sh arquivos-augusto-001 file-handler /home/augusto.scher/git/rep/curso-aws-serverless-apps/07-tooling/sqs-s3-streams/scripts/s3/file.csv
 
 BUCKET_NAME=$1
 QUEUE_NAME=$2
+FILE_PATH=$3
 
-echo "creating bucket..."
+echo "1. deploying..."
+sls deploy
+
+echo "2. creating bucket..."
 aws  \
   s3 mb s3://$BUCKET_NAME \
-  --endpoint-url=http://localhost:4572
+  # --endpoint-url=http://localhost:4572
 
-aws  \
-  s3 ls \
-  --endpoint-url=http://localhost:4572
-
-aws  \
-  s3 ls $BUCKET_NAME \
-  --endpoint-url=http://localhost:4572
-
-echo "creating queue..."
+echo "3. creating queue..."
 
 aws \
   sqs create-queue \
   --queue-name $QUEUE_NAME \
-  --endpoint-url=http://localhost:4576
+  # --endpoint-url=http://localhost:4576
 
 aws \
   sqs list-queues \
-  --endpoint-url=http://localhost:4576
+  # --endpoint-url=http://localhost:4576
+
+echo "4. deploying csv..."
+aws  \
+  s3 cp $FILE_PATH s3://$BUCKET_NAME \
+
+aws  \
+  s3 ls $BUCKET_NAME \
+  # --endpoint-url=http://localhost:4572
