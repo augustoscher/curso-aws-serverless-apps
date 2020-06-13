ebd4c9047d5636467c786a25e150c80dbcbcfa80
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-12 23:35:03 -0300

    08 - GraphQL - Apollo Server - Estrutura base da camada GraphQL genérica

---
+++ 08-graphql-apolloserver/graphql-dynamodb-pt3/src/graphql/index.js
@@ -0,0 +1,21 @@
+const { readdirSync } = require("fs");
+const {
+  makeExecutableSchema,
+  mergeSchemas,
+  gql,
+} = require("apollo-server-lambda");
+
+const schemas = readdirSync(__dirname)
+  //le o diretóiro e descartando este arquivo.
+  .filter((file) => file !== "index.js")
+  //faz o require de cada arquivo index.js dentro da pasta hero, skill...
+  .map((folder) => require(`./${folder}`))
+  //usando o que é retornado de cada index, cria um Schema GraphQL junstando schema e resolver
+  .map(({ schema, resolvers }) =>
+    makeExecutableSchema({
+      //gql serve para validar a string do schema e retornar no formato
+      //Não é obrigatório, mas é recomenddoo
+      typeDefs: gql(schema),
+      resolvers,
+    })
+  );
