5f7fd095b916712738afb3b4946dcb12d27cbdd0
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-13 00:39:00 -0300

    08 - GraphQL - Apollo Server - Estrutura base da camada GraphQL genérica

--- 08-graphql-apolloserver/graphql-dynamodb-pt3/requests.sh
+++ 08-graphql-apolloserver/graphql-dynamodb-pt3/requests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "Request to GraphQL"
+echo "Request to GraphQL - Mutation"
 curl 'https://71pjxh7coa.execute-api.us-east-1.amazonaws.com/dev/graphql' \
   -H 'Content-Type: application/json' \
   --data-binary '{"query":"mutation {\n  createHero\n  createSkill\n}"}' --compressed
+
+echo ""
+echo "Request to GraphQL - Query"
+curl 'https://71pjxh7coa.execute-api.us-east-1.amazonaws.com/dev/graphql' \
+  -H 'Content-Type: application/json' \
+  --data-binary '{"query":"{\n\tgetHero\n  getSkill\n}"}' --compressed
+
