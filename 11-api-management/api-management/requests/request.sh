2bb9e0d5bda2682679dfe18aaad6dcd57ae3ff96
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-28 14:50:09 -0300

    11 - API Keys - Usage Plans - Throttling

--- 11-api-management/api-management/requests/request.sh
+++ 11-api-management/api-management/requests/request.sh
@@ -13,15 +13,29 @@ curl --silent \
 curl --silent \
   $HOST/dev/getUsagePlans | tee requests/getUsagePlans.log
 
+# from getUsagePlans.log
+USAGE_PLAN_ID="73lt09"
 
-
-echo
-echo "------------- PROD --------------"
-echo
-
-HOST_PROD=https://3lgkw2wdw5.execute-api.us-east-1.amazonaws.com
-APIKEY_PROD="Uoi1kz4xIhajfc4IowkCn3wk5tV4D9cM2xx0YlA2"
+# augusto.scher@gmail.com
+KEY_ID=n4x91xh449
+APIKEY="Uoi1kz4xIhajfc4IowkCn3wk5tV4D9cM2xx0YlA2"
+FROM="2020-06-27"
+TO="2020-06-28"
 
 curl --silent \
-  -H "x-api-key: $APIKEY_PROD" \
-  $HOST_PROD/dev/hello
+  "$HOST/dev/getUsage?keyId=$KEY_ID&usagePlanId=$USAGE_PLAN_ID&from=$FROM&to=$TO" \
+  | tee requests/usage.log
+
+
+
+
+# echo
+# echo "------------- PROD --------------"
+# echo
+
+# HOST_PROD=https://3lgkw2wdw5.execute-api.us-east-1.amazonaws.com
+# APIKEY_PROD="Uoi1kz4xIhajfc4IowkCn3wk5tV4D9cM2xx0YlA2"
+
+# curl --silent \
+#   -H "x-api-key: $APIKEY_PROD" \
+#   $HOST_PROD/dev/hello
