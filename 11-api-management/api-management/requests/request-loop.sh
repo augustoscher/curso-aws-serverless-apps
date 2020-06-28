f8f93d5dd5d02a8971d23c89fdb29732cf0c6170
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-28 15:01:24 -0300

    11 - API Keys - Usage Plans - Throttling

--- 11-api-management/api-management/requests/request-loop.sh
+++ 11-api-management/api-management/requests/request-loop.sh
@@ -11,3 +11,17 @@ do
     -H "x-api-key: $APIKEY_PROD" \
     $HOST_PROD/dev/hello
 done
+
+
+# from getUsagePlans.log
+USAGE_PLAN_ID="73lt09"
+
+# augusto.scher@gmail.com
+KEY_ID=n4x91xh449
+APIKEY="Uoi1kz4xIhajfc4IowkCn3wk5tV4D9cM2xx0YlA2"
+FROM="2020-06-27"
+TO="2020-06-28"
+
+curl --silent \
+  "$HOST/dev/getUsage?keyId=$KEY_ID&usagePlanId=$USAGE_PLAN_ID&from=$FROM&to=$TO" \
+  | tee requests/usage.log
