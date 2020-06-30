b0d2c8c033a1a3e4411c02c0bc2e9d95a2d82aed
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-30 13:39:58 -0300

    11 - API Keys - Usage Plans - Throttling

--- 11-api-management/api-management/requests/request-loop.sh
+++ 11-api-management/api-management/requests/request-loop.sh
@@ -3,7 +3,7 @@
 echo "Press <CTRL+C> to exit."
 
 HOST_PROD=https://3lgkw2wdw5.execute-api.us-east-1.amazonaws.com
-APIKEY_PROD="Uoi1kz4xIhajfc4IowkCn3wk5tV4D9cM2xx0YlA2"
+APIKEY_PROD="dxyB4oINNu1IM6kub06E24nT3LD8SwxX7aXQU25b"
 
 while :
 do
--- 11-api-management/api-management/requests/request-prod.sh
+++ 11-api-management/api-management/requests/request-prod.sh
@@ -3,10 +3,15 @@
 HOST=https://3lgkw2wdw5.execute-api.us-east-1.amazonaws.com
 APIKEY="Uoi1kz4xIhajfc4IowkCn3wk5tV4D9cM2xx0YlA2"
 
-# curl --silent \
-#   -H "x-api-key: $APIKEY" \
-#   $HOST/dev/hello
+## Calling Hello 
+curl --silent \
+  -H "x-api-key: $APIKEY" \
+  $HOST/dev/hello
 
+
+## Get usage plans
+curl --silent \
+  $HOST/dev/getUsagePlans
 
 # from getUsagePlans.log
 USAGE_PLAN_ID="73lt09"
@@ -17,15 +22,40 @@ curl --silent \
   "$HOST/dev/addkey?name=$CUSTOMER_NAME&usagePlanId=$USAGE_PLAN_ID" \
   | tee requests/addKey.log
 
+# Return:
+# {
+#   "apiKeyToken": "4HHxRhzVwN65xWWzSCxJb2GKpQqKQtsIvOyS5UA5",
+#   "apiKeyId": "ewqydrrc1b",
+#   "message": "Use ewqydrrc1b to check quota and 'x-api-key: 4HHxRhzVwN65xWWzSCxJb2GKpQqKQtsIvOyS5UA5' to make requests."
+# }
 
 ## Get usage
-KEY_ID=n4x91xh449
-APIKEY="Uoi1kz4xIhajfc4IowkCn3wk5tV4D9cM2xx0YlA2"
+KEY_ID=ewqydrrc1b
+APIKEY="4HHxRhzVwN65xWWzSCxJb2GKpQqKQtsIvOyS5UA5"
 FROM="2020-06-27"
-TO="2020-06-28"
+TO="2020-06-30"
 
 curl --silent \
   "$HOST/dev/getUsage?keyId=$KEY_ID&usagePlanId=$USAGE_PLAN_ID&from=$FROM&to=$TO" \
   | tee requests/usage.log
 
+# Return:
+# {
+#   "usagePlanId": "73lt09",
+#   "startDate": "2020-06-27",
+#   "endDate": "2020-06-30",
+#   "items": {}
+# }
 
+## Hitting on /dev/hello again to raise limit
+while :
+do
+  curl --silent \
+    -H "x-api-key: $APIKEY" \
+    $HOST/dev/hello
+done
+
+
+## Get usage again
+curl --silent \
+  "$HOST/dev/getUsage?keyId=$KEY_ID&usagePlanId=$USAGE_PLAN_ID&from=$FROM&to=$TO"
