25a8ec79511dd8c0b4d924daafb2163407547949
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-06-30 13:40:15 -0300

    11 - API Keys - Usage Plans - Throttling

--- 11-api-management/api-management/requests/request-loop.sh
+++ 11-api-management/api-management/requests/request-loop.sh
@@ -2,12 +2,12 @@
 
 echo "Press <CTRL+C> to exit."
 
-HOST_PROD=https://3lgkw2wdw5.execute-api.us-east-1.amazonaws.com
-APIKEY_PROD="dxyB4oINNu1IM6kub06E24nT3LD8SwxX7aXQU25b"
+HOST=https://3lgkw2wdw5.execute-api.us-east-1.amazonaws.com
+APIKEY="dxyB4oINNu1IM6kub06E24nT3LD8SwxX7aXQU25b"
 
 while :
 do
   curl --silent \
-    -H "x-api-key: $APIKEY_PROD" \
-    $HOST_PROD/dev/hello
+    -H "x-api-key: $APIKEY" \
+    $HOST/dev/hello
 done
