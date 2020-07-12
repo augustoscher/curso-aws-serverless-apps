2075e0bad07c3f53fa48bf5caa64ca5922f7e6f4
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-07-12 01:03:56 -0300

    12 - RDS Aurora Serverless + MySQL + Sequelize + AWS VPC

--- 12-vpc-aurora-serverless/vpc-aurora-sql/src/index.js
+++ 12-vpc-aurora-serverless/vpc-aurora-sql/src/index.js
@@ -1,20 +1,38 @@
-const {
-  HeroSchema,
-  sequelize
-} = require('./database');
+const { HeroSchema, sequelize } = require("./database");
 
-const handler = async event => {
-  console.log('oi')
+const faker = require("faker");
+
+const handler = async (event) => {
+  console.log("oi");
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.')
+    console.log("Connection has been established successfully.");
   } catch (error) {
-    console.log('Unable to connect to the database', error.stack);
+    console.log("Unable to connect to the database", error.stack);
     return {
       statusCode: 500,
-      body: 'Internal Server Error'
-    }
+      body: "Internal Server Error",
+    };
   }
-}
+
+  await HeroSchema.sync();
+  const result = await HeroSchema.create({
+    name: faker.name.title(),
+    power: faker.name.jobTitle(),
+  });
+
+  const all = await HeroSchema.findAll({
+    raw: true,
+    attributes: ["id", "name", "power"],
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      result,
+      all,
+    }),
+  };
+};
 
 exports.handler = handler;
