d40ad652e92510f202aa6267591bd52b4c6a2bdd
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-07-25 16:27:05 -0300

    13 - Long Running Process - ECS + Fargate

--- 13-high-processing/config/app/run.sh
+++ 13-high-processing/config/app/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 IMAGE_URL="824273212766.dkr.ecr.us-east-1.amazonaws.com/process-data"
+REGION="us-east-1"
 
 docker build -t $IMAGE_URL .
 
 docker run $IMAGE_URL
+
+aws ecr get-login --no-include-email --region $REGION | bash
+
+docker push $IMAGE_URL
