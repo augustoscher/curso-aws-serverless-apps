021f4a21344a468dd33019ac04ef1a3ef235c73e
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-07-25 16:06:11 -0300

    13 - Long Running Process - ECS + Fargate

--- 13-high-processing/config/infra/setup-fargate.sh
+++ 13-high-processing/config/infra/setup-fargate.sh
@@ -15,6 +15,14 @@ CUSTOM_POLICY_ARN="arn:aws:iam::824273212766:policy/process-data-policy"
 ECR_URI_DOCKER="824273212766.dkr.ecr.us-east-1.amazonaws.com/process-data"
 SSM_ENV_PATH="/prod/$PROJECT_NAME/"
 
+TASK_DEFINITION_ARN="arn:aws:ecs:us-east-1:824273212766:task-definition/process-data:1"
+
+VPC_ID="vpc-51c67a2b"
+SECURITY_GROUP_NAME="$PROJECT_NAME"
+GROUP_ID="sg-020a0f6774e30864d"
+
+
+
 aws iam create-role \
   --region $REGION \
   --role-name $ECS_ROLE_NAME \
@@ -65,7 +73,37 @@ aws ecr create-repository \
   --region $REGION \
   | tee logs/5.create-docker-repo.txt
 
+# Criando definição do container
+
 aws ecs register-task-definition \
   --cli-input-json file://templates/task-definition.json \
   | tee logs/6.register-task.txt
 
+# Listando containers definidos
+
+aws ecs list-task-definitions \
+  | tee logs/7.tasks-definitions.txt
+
+# Security
+
+aws ec2 describe-vpcs \
+  | tee logs/8.describe-vpcs.txt
+
+aws ec2 describe-subnets \
+  --filters="Name=vpc-id,Values=$VPC_ID" \
+  --query "Subnets[*].SubnetId" \
+  | tee logs/9.describe-subnets.txt
+
+aws ec2 create-security-group \
+  --group-name $SECURITY_GROUP_NAME \
+  --description "grupo de acesso em ecs tasks" \
+  | tee logs/10.create-security-group.txt
+
+
+aws ec2 authorize-security-group-ingress \
+  --group-id $GROUP_ID \
+  --protocol tcp \
+  --port 80 \
+  --cidr 0.0.0.0/0 \
+  --region $REGION \
+  | tee logs/11.authorize-sec-group.txt
