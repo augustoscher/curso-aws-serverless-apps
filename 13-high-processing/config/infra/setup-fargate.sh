70be81646e77ff89f942aa3ba19abbdf22541ed1
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-07-25 15:17:07 -0300

    13 - Long Running Process - ECS + Fargate

--- 13-high-processing/config/infra/setup-fargate.sh
+++ 13-high-processing/config/infra/setup-fargate.sh
@@ -7,7 +7,10 @@ REGION="us-east-1"
 LOG_GROUP_NAME=""
 
 ECS_ROLE_NAME="ecsTaskExecutionRole"
-ECS_ROLE_ARN=""
+ECS_ROLE_ARN="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+
+CUSTOM_POLICY_NAME="$APP_NAME"-policy
+CUSTOM_POLICY_ARN="arn:aws:iam::824273212766:policy/process-data-policy"
 
 aws iam create-role \
   --region $REGION \
@@ -15,3 +18,26 @@ aws iam create-role \
   --assume-role-policy-document file://templates/task-execution-assume-role.json \
   | tee logs/1.iam-create-role.txt
 
+# dar permissão de executar chamadas ecs na role
+aws iam attach-role-policy \
+  --region $REGION \
+  --role-name $ECS_ROLE_NAME \
+  --policy-arn $ECS_ROLE_ARN
+
+# permissão para a instancia acessar o s3 e variaveis de ambiente
+# - acessar o bucket surveys
+# - fazer download do csv de surveys
+# - fazer upload para a pasta surveys/reports
+# - ler as variáveis de System Management Store
+
+aws iam create-policy \
+  --policy-name $CUSTOM_POLICY_NAME \
+  --policy-document file://templates/custom-access-policy.json \
+  | tee logs/2.create-policy.txt
+
+# Adicionar policy acima na role principal
+
+aws iam attach-role-policy \
+  --region $REGION \
+  --role-name $ECS_ROLE_NAME \
+  --policy-arn $CUSTOM_POLICY_ARN
