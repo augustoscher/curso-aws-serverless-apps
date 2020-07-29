2fe4de4710d9367da32cd88fab83af5f1aa233d1
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-07-29 13:44:37 -0300

    13 - Long Running Process - ECS + Fargate + System Manager

---
+++ 13-high-processing/config/env-manager/env.js
@@ -0,0 +1,20 @@
+// APP_NAME="process-data"
+// CLUSTER_NAME="serverless-project"
+// PROJECT_NAME="serverless-project-01"
+// REGION="us-east-1"
+// LOG_GROUP_NAME="/ecs/$PROJECT_NAME"
+
+// ECS_ROLE_NAME="ecsTaskExecutionRole"
+// ECS_ROLE_ARN="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+
+// CUSTOM_POLICY_NAME="$APP_NAME"-policy
+// CUSTOM_POLICY_ARN="arn:aws:iam::824273212766:policy/process-data-policy"
+
+// ECR_URI_DOCKER="824273212766.dkr.ecr.us-east-1.amazonaws.com/process-data"
+// SSM_ENV_PATH="/prod/$PROJECT_NAME/"
+
+// TASK_DEFINITION_ARN="arn:aws:ecs:us-east-1:824273212766:task-definition/process-data:1"
+
+// VPC_ID="vpc-51c67a2b"
+// SECURITY_GROUP_NAME="$PROJECT_NAME"
+// GROUP_ID="sg-020a0f6774e30864d"
